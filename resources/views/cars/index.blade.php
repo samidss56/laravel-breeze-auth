ceee7ff0ecbb654a9b9cecd51f0e7b27a2e14ce0
samidss56 <adimas.id@gmail.com>
2024-04-01 22:19:22 +0700

    laravel breeze authentication

---
+++ resources/views/cars/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Index cars</title>
+</head>
+<body>
+    <h1>cars Page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet ea sed corrupti ipsa itaque consectetur qui tempora numquam nostrum ut temporibus, eum laudantium magnam a ipsum cum veniam. Veritatis, delectus.</p>
+</body>
+</html>
